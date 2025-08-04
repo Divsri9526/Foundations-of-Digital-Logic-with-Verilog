2a89c86d630468b482b586fd647b86fbe16361a0
Divyansh Srivastava <divyanshsri108@gmail.com>
2025-08-04 19:03:30 +0530

    Add files via upload

---
+++ and_gate_testbench.sv
@@ -0,0 +1,20 @@
+module and_gate_tb;
+  
+  reg a,b;
+  wire y;
+  
+  and_gate uut (a,b,y);
+  
+  initial begin
+    $dumpfile("waveform.vcd");
+    $dumpvars(0, and_gate_tb);
+    
+    $display("A B | Y");
+    a = 0; b= 0;#5 $display("%b %b | %b", a, b, y);
+    a = 0; b= 1;#5 $display("%b %b | %b", a, b, y);
+    a = 1; b= 0;#5 $display("%b %b | %b", a, b, y);
+    a = 1; b= 1;#5 $display("%b %b | %b", a, b, y);
+    $finish;
+  end
+  
+endmodule
